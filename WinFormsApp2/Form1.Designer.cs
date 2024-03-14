a6c60306ccd5b22e6f426c732505c3518bef0e4c
Faiz <a.faizmail@gmail.com>
2024-03-14 16:04:27 +0700

    Cell Click

--- WinFormsApp2/Form1.Designer.cs
+++ WinFormsApp2/Form1.Designer.cs
@@ -28,128 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.gvSuperHeroes = new System.Windows.Forms.DataGridView();
-            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.superHeroBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtFirstName = new System.Windows.Forms.TextBox();
-            this.txtLastName = new System.Windows.Forms.TextBox();
-            this.btnSave = new System.Windows.Forms.Button();
-            this.txtBoxAutocomplete = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.gvSuperHeroes)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.superHeroBindingSource)).BeginInit();
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            gvSuperHeroes = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            firstNameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            lastNameDataGridViewTextBoxColumn = new DataGridViewTextBoxColumn();
+            superHeroBindingSource = new BindingSource(components);
+            label1 = new Label();
+            label2 = new Label();
+            txtFirstName = new TextBox();
+            txtLastName = new TextBox();
+            btnSave = new Button();
+            txtBoxAutocomplete = new TextBox();
+            btnDelete = new Button();
+            ((System.ComponentModel.ISupportInitialize)gvSuperHeroes).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)superHeroBindingSource).BeginInit();
+            SuspendLayout();
             // 
             // gvSuperHeroes
             // 
-            this.gvSuperHeroes.AllowUserToAddRows = false;
-            this.gvSuperHeroes.AllowUserToDeleteRows = false;
-            this.gvSuperHeroes.AutoGenerateColumns = false;
-            this.gvSuperHeroes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gvSuperHeroes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.firstNameDataGridViewTextBoxColumn,
-            this.lastNameDataGridViewTextBoxColumn});
-            this.gvSuperHeroes.DataSource = this.superHeroBindingSource;
-            this.gvSuperHeroes.Location = new System.Drawing.Point(0, 0);
-            this.gvSuperHeroes.Name = "gvSuperHeroes";
-            this.gvSuperHeroes.RowHeadersWidth = 51;
-            this.gvSuperHeroes.RowTemplate.Height = 29;
-            this.gvSuperHeroes.Size = new System.Drawing.Size(483, 269);
-            this.gvSuperHeroes.TabIndex = 0;
+            gvSuperHeroes.AllowUserToAddRows = false;
+            gvSuperHeroes.AllowUserToDeleteRows = false;
+            gvSuperHeroes.AutoGenerateColumns = false;
+            gvSuperHeroes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            gvSuperHeroes.Columns.AddRange(new DataGridViewColumn[] { Column1, firstNameDataGridViewTextBoxColumn, lastNameDataGridViewTextBoxColumn });
+            gvSuperHeroes.DataSource = superHeroBindingSource;
+            gvSuperHeroes.Location = new Point(0, 0);
+            gvSuperHeroes.Margin = new Padding(3, 2, 3, 2);
+            gvSuperHeroes.Name = "gvSuperHeroes";
+            gvSuperHeroes.ReadOnly = true;
+            gvSuperHeroes.RowHeadersWidth = 51;
+            gvSuperHeroes.RowTemplate.Height = 29;
+            gvSuperHeroes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            gvSuperHeroes.Size = new Size(423, 202);
+            gvSuperHeroes.TabIndex = 0;
+            gvSuperHeroes.CellClick += gvSuperHeroes_CellClick;
+            gvSuperHeroes.SelectionChanged += gvSuperHeroes_SelectionChanged;
+            // 
+            // Column1
+            // 
+            Column1.DataPropertyName = "Id";
+            Column1.HeaderText = "ID";
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
             // 
             // firstNameDataGridViewTextBoxColumn
             // 
-            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
-            this.firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
-            this.firstNameDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
-            this.firstNameDataGridViewTextBoxColumn.Width = 125;
+            firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
+            firstNameDataGridViewTextBoxColumn.HeaderText = "FirstName";
+            firstNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            firstNameDataGridViewTextBoxColumn.ReadOnly = true;
+            firstNameDataGridViewTextBoxColumn.Width = 125;
             // 
             // lastNameDataGridViewTextBoxColumn
             // 
-            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
-            this.lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
-            this.lastNameDataGridViewTextBoxColumn.MinimumWidth = 6;
-            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
-            this.lastNameDataGridViewTextBoxColumn.Width = 125;
+            lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
+            lastNameDataGridViewTextBoxColumn.HeaderText = "LastName";
+            lastNameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
+            lastNameDataGridViewTextBoxColumn.ReadOnly = true;
+            lastNameDataGridViewTextBoxColumn.Width = 125;
             // 
             // superHeroBindingSource
             // 
-            this.superHeroBindingSource.DataSource = typeof(WinFormsApp2.Model.SuperHero);
+            superHeroBindingSource.DataSource = typeof(Model.SuperHero);
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 287);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "First Name";
+            label1.AutoSize = true;
+            label1.Location = new Point(10, 215);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 15);
+            label1.TabIndex = 1;
+            label1.Text = "First Name";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(250, 293);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Last Name";
+            label2.AutoSize = true;
+            label2.Location = new Point(219, 220);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Last Name";
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(10, 316);
-            this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(213, 27);
-            this.txtFirstName.TabIndex = 3;
+            txtFirstName.Location = new Point(9, 237);
+            txtFirstName.Margin = new Padding(3, 2, 3, 2);
+            txtFirstName.Name = "txtFirstName";
+            txtFirstName.Size = new Size(187, 23);
+            txtFirstName.TabIndex = 3;
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(250, 316);
-            this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(233, 27);
-            this.txtLastName.TabIndex = 4;
+            txtLastName.Location = new Point(219, 237);
+            txtLastName.Margin = new Padding(3, 2, 3, 2);
+            txtLastName.Name = "txtLastName";
+            txtLastName.Size = new Size(204, 23);
+            txtLastName.TabIndex = 4;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(10, 360);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(473, 29);
-            this.btnSave.TabIndex = 5;
-            this.btnSave.Text = "Simpan";
-            this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            btnSave.Location = new Point(9, 270);
+            btnSave.Margin = new Padding(3, 2, 3, 2);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(187, 22);
+            btnSave.TabIndex = 5;
+            btnSave.Text = "Simpan";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
             // 
             // txtBoxAutocomplete
             // 
-            this.txtBoxAutocomplete.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
-            this.txtBoxAutocomplete.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
-            this.txtBoxAutocomplete.Location = new System.Drawing.Point(598, 77);
-            this.txtBoxAutocomplete.Name = "txtBoxAutocomplete";
-            this.txtBoxAutocomplete.Size = new System.Drawing.Size(184, 27);
-            this.txtBoxAutocomplete.TabIndex = 6;
+            txtBoxAutocomplete.AutoCompleteMode = AutoCompleteMode.Suggest;
+            txtBoxAutocomplete.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            txtBoxAutocomplete.Location = new Point(523, 58);
+            txtBoxAutocomplete.Margin = new Padding(3, 2, 3, 2);
+            txtBoxAutocomplete.Name = "txtBoxAutocomplete";
+            txtBoxAutocomplete.Size = new Size(162, 23);
+            txtBoxAutocomplete.TabIndex = 6;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Enabled = false;
+            btnDelete.Location = new Point(221, 269);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(202, 23);
+            btnDelete.TabIndex = 7;
+            btnDelete.Text = "Delete";
+            btnDelete.TextImageRelation = TextImageRelation.ImageAboveText;
+            btnDelete.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(972, 407);
-            this.Controls.Add(this.txtBoxAutocomplete);
-            this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.txtLastName);
-            this.Controls.Add(this.txtFirstName);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.gvSuperHeroes);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.gvSuperHeroes)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.superHeroBindingSource)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(850, 305);
+            Controls.Add(btnDelete);
+            Controls.Add(txtBoxAutocomplete);
+            Controls.Add(btnSave);
+            Controls.Add(txtLastName);
+            Controls.Add(txtFirstName);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(gvSuperHeroes);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)gvSuperHeroes).EndInit();
+            ((System.ComponentModel.ISupportInitialize)superHeroBindingSource).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -160,9 +190,11 @@
         private TextBox txtFirstName;
         private TextBox txtLastName;
         private Button btnSave;
-        private DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
-        private DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
         private BindingSource superHeroBindingSource;
         private TextBox txtBoxAutocomplete;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
+        private DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
+        private Button btnDelete;
     }
 }
--- WinFormsApp2/Form1.cs
+++ WinFormsApp2/Form1.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using System.Windows.Forms;
 using WinFormsApp2.Model;
 
 namespace WinFormsApp2
@@ -6,6 +8,7 @@ namespace WinFormsApp2
     public partial class Form1 : Form
     {
         private DataContext? _context;
+        private int selectedId;
 
         public Form1()
         {
@@ -26,6 +29,7 @@ namespace WinFormsApp2
             AutoCompleteStringCollection src = new AutoCompleteStringCollection();
             src.AddRange(autoCompleteSource);
             txtBoxAutocomplete.AutoCompleteCustomSource = src;
+            Console.WriteLine("Test");
         }
 
         protected override void OnClosed(EventArgs e)
@@ -43,9 +47,38 @@ namespace WinFormsApp2
             newHero.LastName = txtLastName.Text;
 
             _context.SuperHeroes.Add(newHero); // menambahkan data ke DbSet<SuperHero>
-            
+
             // simpan prubahan
             await _context.SaveChangesAsync();
+        }
+
+        private void gvSuperHeroes_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Int32 selectedRowCount =
+        gvSuperHeroes.Rows.GetRowCount(DataGridViewElementStates.Selected);
+
+            MessageBox.Show(selectedRowCount.ToString());
+        }
+
+        private void gvSuperHeroes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (gvSuperHeroes.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = gvSuperHeroes.SelectedRows[0];
+                Debug.WriteLine(gvSuperHeroes.Rows[selectedRow.Index].Cells[0].Value);
+                selectedId = 1;
+                btnDelete.Enabled = true;
+            } else
+            {
+                btnDelete.Enabled = false;
+            }
+
+
+        }
+
+        private void gvSuperHeroes_SelectionChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
